44897c0916f787712b0f8525a633c41226223a3c
Irina Nicula <irinanicula@Irinas-MacBook-Pro-2.local>
2024-06-18 15:49:04 +0200

    Added a container/possible carousel for the filtered tv shows

---
+++ src/components/TvShowCarousel.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+defineProps({
+  genre: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+    <h2>{{ genre }}</h2>
+    <div class="carousel">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .carousel {
+        display: flex;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useTvShowsStore } from '@/stores/tvShows';
 import TvShowCard from '@/components/TvShowCard.vue';
+import TvShowCarousel from '@/components/TvShowCarousel.vue';
 
 const tvShowStore = useTvShowsStore();
 tvShowStore.getTvShows();
@@ -9,55 +10,55 @@ tvShowStore.getTvShows();
 <template>
   <main>
     <h3>List of tv-shows by genre ordered by rating</h3>
-    <h4>Drama:</h4>
-    <ul>
-      <tv-show-card
+      <tv-show-carousel genre="Drama">
+        <tv-show-card
         v-for="show in tvShowStore.sortTvShowsByRating('Drama')"
+        :id="show.id"
         :key="show.id"
         :name="show.name"
         :rating="show.rating.average"
         :imageSrc="show.image.medium"
       ></tv-show-card>
-    </ul>
-    <h4>Comedy:</h4>
-    <ul>
-      <tv-show-card
+    </tv-show-carousel>
+    <tv-show-carousel genre="Comedy">
+        <tv-show-card
         v-for="show in tvShowStore.sortTvShowsByRating('Comedy')"
+        :id="show.id"
         :key="show.id"
         :name="show.name"
         :rating="show.rating.average"
         :imageSrc="show.image.medium"
       ></tv-show-card>
-    </ul>
-    <h4>Science-fiction:</h4>
-    <ul>
-      <tv-show-card
+    </tv-show-carousel>
+    <tv-show-carousel genre="Science-Fiction">
+        <tv-show-card
         v-for="show in tvShowStore.sortTvShowsByRating('Science-Fiction')"
+        :id="show.id"
         :key="show.id"
         :name="show.name"
         :rating="show.rating.average"
         :imageSrc="show.image.medium"
       ></tv-show-card>
-    </ul>
-    <h4>Fantasy:</h4>
-    <ul>
-      <tv-show-card
-        v-for="show in tvShowStore.sortTvShowsByRating('Science-Fiction')"
+    </tv-show-carousel>
+    <tv-show-carousel genre="Fantasy">
+        <tv-show-card
+        v-for="show in tvShowStore.sortTvShowsByRating('Fantasy')"
+        :id="show.id"
         :key="show.id"
         :name="show.name"
         :rating="show.rating.average"
         :imageSrc="show.image.medium"
       ></tv-show-card>
-    </ul>
-    <h4>Sports:</h4>
-    <ul>
-      <tv-show-card
+    </tv-show-carousel>
+    <tv-show-carousel genre="Sports">
+        <tv-show-card
         v-for="show in tvShowStore.sortTvShowsByRating('Sports')"
+        :id="show.id"
         :key="show.id"
         :name="show.name"
         :rating="show.rating.average"
         :imageSrc="show.image.medium"
       ></tv-show-card>
-    </ul>
+    </tv-show-carousel>
   </main>
 </template>
